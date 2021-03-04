6c1b459545657a745d99eceefd6db920253e8e45
charkster <31560568+charkster@users.noreply.github.com>
2021-03-03 17:12:13 -0700

    Update test_mcp4728.py

--- test_mcp4728.py
+++ test_mcp4728.py
@@ -18,7 +18,7 @@ dac_x4.ch2_vout = 2.1
 dac_x4.ch3_gain = 1
 dac_x4.ch3_pd   = 0
 dac_x4.ch3_vout = 0.8
-dac_x4.update_values() # this is not really needed as multi_write() calls update_values() before doign the write
+dac_x4.update_values() # this is not really needed as multi_write() calls update_values() before doing the write
 dac_x4.multi_write(ch2=True, ch3=True) # selected which channels to write to
 dac_x4.read_and_print()
 dac_x4.write_eeprom_all_off() # configure registers and eeprom to have all channels off
